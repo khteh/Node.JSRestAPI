a94e9c6962b3feecd1729d644d632b6ab518f391
Teh Kok How <funcoolgeek@gmail.com>
2019-04-20 19:18:45 +0800

    (1) Add school_db.sql (2) Use shcool_db.sql in .circleci DB init

---
+++ school_db.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE school /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE DATABASE school;
+CREATE TABLE teachers (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  email varchar(320) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id_UNIQUE (id),
+  KEY Non-Clustered (email)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE students (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  email varchar(320) NOT NULL,
+  isSuspended tinyint(4) NOT NULL DEFAULT '0',
+  UNIQUE KEY id_UNIQUE (id),
+  KEY Non-Clustered (email)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE teacher_student (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  teacherid int(11) NOT NULL,
+  studentid int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY teacherid_idx (teacherid),
+  KEY studentid_idx (studentid),
+  CONSTRAINT studentid FOREIGN KEY (studentid) REFERENCES students (id),
+  CONSTRAINT teacherid FOREIGN KEY (teacherid) REFERENCES teachers (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
