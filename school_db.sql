b2126b505c9ef1b080d04f2207fc94b6494bea87
Teh Kok How <funcoolgeek@gmail.com>
2019-04-20 22:03:22 +0800

    NONCLUSTERED

--- school_db.sql
+++ school_db.sql
@@ -5,14 +5,14 @@ CREATE TABLE teachers (
   email varchar(320) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY id_UNIQUE (id),
-  KEY Non-Clustered ('email')
+  KEY NONCLUSTERED (email)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE students (
   id int(11) NOT NULL AUTO_INCREMENT,
   email varchar(320) NOT NULL,
   isSuspended tinyint(4) NOT NULL DEFAULT '0',
   UNIQUE KEY id_UNIQUE (id),
-  KEY Non-Clustered ('email')
+  KEY NONCLUSTERED (email)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE teacher_student (
   id int(11) NOT NULL AUTO_INCREMENT,
