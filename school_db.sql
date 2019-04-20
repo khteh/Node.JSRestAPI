54e2817bf537355efe89cd784b0953ccc53980f6
Teh Kok How <funcoolgeek@gmail.com>
2019-04-20 22:44:12 +0800

    (1) updated node_modules (2) Use school_test for tests

--- school_db.sql
+++ school_db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE school /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE school;
+CREATE DATABASE school_test /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE school_test;
 CREATE TABLE teachers (
   id int(11) NOT NULL AUTO_INCREMENT,
   email varchar(320) NOT NULL,
