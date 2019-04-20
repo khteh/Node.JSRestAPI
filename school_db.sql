fe470e2cbba43ca760146a7fb3a06949aa7ee670
Teh Kok How <funcoolgeek@gmail.com>
2019-04-20 23:43:52 +0800

    Add alter user

--- school_db.sql
+++ school_db.sql
@@ -1,4 +1,5 @@
-CREATE USER 'guest'@'localhost' IDENTIFIED WITH mysql_native_password BY 'P@$$w0rd';
+CREATE USER 'guest'@'%' IDENTIFIED WITH mysql_native_password BY 'P@$$w0rd';
+alter user 'guest'@'%' identified with mysql_native_password BY 'P@$$w0rd';
 CREATE DATABASE school_test /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 USE school_test;
 CREATE TABLE teachers (
@@ -25,5 +26,5 @@ CREATE TABLE teacher_student (
   CONSTRAINT studentid FOREIGN KEY (studentid) REFERENCES students (id),
   CONSTRAINT teacherid FOREIGN KEY (teacherid) REFERENCES teachers (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-grant all on school_test.* to 'guest'@'localhost';
+grant all on school_test.* to 'guest'@'%';
 FLUSH PRIVILEGES;
