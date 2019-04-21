dea0ec4a93f0435a8a8a8a289bac3da2b77b9ce9
Teh Kok How <funcoolgeek@gmail.com>
2019-04-21 19:30:36 +0800

    Move mocha, eslint and nyc to devDependencies

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var api = require('./routes/api');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
