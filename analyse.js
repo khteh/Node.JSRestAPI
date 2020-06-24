8e0c6470e5826f5e4ad62bad2201098b2c38d333
Teh Kok How <funcoolgeek@gmail.com>
2020-06-24 15:19:18 +0800

    SonarQube config and test successful

--- analyse.js
+++ analyse.js
@@ -1,18 +1,24 @@
 const scanner = require("sonarqube-scanner");
-
 scanner(
   {
-    // this example uses local instance of SQ
-    serverUrl: "http://localhost:9000/",
+    serverUrl: "http://localhost:9000",
+	token: "8ebefd6b851b8ae7d248a6db2be2393bad1f8d7c",
     options: {
-      "sonar.projectVersion": "1.1.0",
-      "sonar.sources": "src",
-      "sonar.tests": "tests",
-      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
-      "sonar.testExecutionReportPaths": "test-report.xml"
-    },
+		"sonar.projectKey": "Node.JSRestAPI",
+		"sonar.projectVersion": "1.0.0",
+		"sonar.sourceEncoding": "UTF-8",
+		"sonar.log.level": "TRACE",
+		"sonar.verbose": "true",
+		"sonar.scm.disabled": "true",
+		"sonar.sources": "BusinessLogic, routes, bin, app.js",
+		"sonar.tests": "tests",
+		"sonar.language": "null",
+		"sonar.dependencyCheck.reportPath": "dependency-check-report.xml",
+		"sonar.dependencyCheck.htmlReportPath": "dependency-check-report.html",
+		"sonar.javascript.jstest.reportsPath": "test_reports",
+		"sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
+		"sonar.testExecutionReportPaths": "test_reports/sonarqube/test_results.xml"
+    }
   },
-  () => {
-    // callback is required
-  }
+  () => process.exit()
 );
