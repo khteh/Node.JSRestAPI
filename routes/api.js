c4ae2c0e95185036c29d333df8db6a06ca3551f1
Kok How, Teh <funcoolgeek@gmail.com>
2021-10-12 11:37:44 +0800

    Add fibonacci calculation api endpoint

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,9 @@ import notifications from '../BusinessLogic/notifications.js'
 import suspend from '../BusinessLogic/suspend.js'
 import commonstudents from '../BusinessLogic/commonstudents.js'
 import greetings from '../BusinessLogic/greetings.js'
+import fibonacci from '../BusinessLogic/fibonacci.js'
 router.get('/greetings', function (req, res, next) { greetings(req, res, next); });
+router.get('/fibonacci', function (req, res, next) { fibonacci(req, res, next); });
 router.post('/register', function (req, res, next) { registration(req, res, next); });
 router.post('/retrievefornotifications', function (req, res, next) { notifications(req, res, next); });
 router.get('/commonstudents', function (req, res, next) { commonstudents(req, res, next); });
