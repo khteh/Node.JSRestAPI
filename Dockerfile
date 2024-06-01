e46e9756f0d6a9c9250f3147cee623e098b80bba
Kok How, Teh <funcoolgeek@gmail.com>
2024-06-01 11:06:34 +0800

    Improve TLS cert generation scripts

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ ADD package.json .
 ADD tsconfig.json .
 ADD node_modules node_modules
 ADD build .
-RUN openssl req -new -newkey rsa:4096 -x509 -nodes -days 365 -keyout server.key -out server.crt -subj "/C=SG/ST=Singapore/L=Singapore /O=Kok How Pte. Ltd./OU=NodeJSRestAPI/CN=localhost/emailAddress=funcoolgeek@gmail.com" -passin pass:NodeJSRestAPI
+RUN openssl req -new -newkey rsa:4096 -x509 -nodes -days 365 -keyout server.key -out server.crt -subj "/C=SG/ST=Singapore/L=Singapore /O=Kok How Pte. Ltd./OU=Node.JSRestAPI/CN=localhost/emailAddress=funcoolgeek@gmail.com" -passin pass:Node.JSRestAPI -addext "subjectAltName = DNS:localhost"
 EXPOSE 443
 ENTRYPOINT ["node", "--trace-deprecation", "--experimental-specifier-resolution=node", "src/webapi/server.js"]
--- tls.sh
+++ tls.sh
@@ -3,5 +3,5 @@ openssl genrsa -aes256 -passout pass:Node.JSRestAPI -out server.key 4096
 openssl rsa -in server.key -out server.key.insecure -passin pass:Node.JSRestAPI
 mv server.key server.key.secure
 mv server.key.insecure server.key
-openssl req -new -newkey rsa:4096 -x509 -nodes -days 365 -keyout server.key -out server.crt -subj "/C=SG/ST=Singapore/L=Singapore /O=Node.JSRestAPI Pte. Ltd./OU=Node.JSRestAPI/CN=Node.JSRestAPI/emailAddress=me@email.com" -passin pass:Node.JSRestAPI
-openssl pkcs12 -export -out /tmp/localhost.pfx -inkey server.key -in server.crt -certfile server.crt -passout pass:Node.JSRestAPI
+openssl req -new -newkey rsa:4096 -x509 -nodes -days 365 -keyout server.key -out server.crt -subj "/C=SG/ST=Singapore/L=Singapore /O=Kok How Pte. Ltd./OU=Node.JSRestAPI/CN=localhost/emailAddress=funcoolgeek@gmail.com" -passin pass:Node.JSRestAPI -addext "subjectAltName = DNS:localhost"
+openssl pkcs12 -export -out /tmp/localhost.pfx -inkey server.key -in server.crt -passout pass:Node.JSRestAPI
