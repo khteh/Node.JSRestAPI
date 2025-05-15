22bf5f7b39b974eb492354f25f39869c74a9f767
Kok How, Teh <funcoolgeek@gmail.com>
2025-05-15 11:10:32 +0800

    Fix Google GenAI API changes. Process JSON code block

--- src/webapi.core/UseCases/GenerateTextUseCase.ts
+++ src/webapi.core/UseCases/GenerateTextUseCase.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { GoogleGenerativeAI, GenerativeModel, Part } from "@google/generative-ai";
+import { GoogleGenAI, createUserContent, Part, GenerateContentResponse } from "@google/genai";
 import { IGenerateTextUseCase } from "../Interfaces/UseCases/IGenerateTextUseCase.js"
 import { IOutputPort } from "../Interfaces/IOutputPort.js";
 import { UseCaseResponseMessage } from "../DTO/UseCaseResponse/UseCaseResponseMessage.js"
@@ -12,12 +12,10 @@ import e from "express";
 @injectable()
 export class GenerateTextUseCase implements IGenerateTextUseCase {
     private _logger: ILogger;
-    private readonly _genAI: GoogleGenerativeAI;
-    private readonly _model: GenerativeModel;
+    private readonly _genAI: GoogleGenAI;
     public constructor(@inject(LoggerTypes.ILogger) logger: ILogger) {
         this._logger = logger;
-        this._genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-        this._model = this._genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+        this._genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY! });
     }
     private FileToGenerativePart (path: string, mimeType: string) {
         return {
@@ -34,19 +32,37 @@ export class GenerateTextUseCase implements IGenerateTextUseCase {
         try {
             this._logger.Log(LogLevels.debug, 'POST /api/gemini query: ' + JSON.stringify(request, null, 2));
             if (request.Prompt) {
-                let result;
+                let result: GenerateContentResponse;
                 if (request.Image) {
                     this._logger.Log(LogLevels.debug, `Reading uploaded file from ${request.Image.Path}`);
                     let imagePart: Part = this.FileToGenerativePart(request.Image.Path, request.Image.MimeType);
-                    result = await this._model.generateContent([request.Prompt, imagePart]);
+                    result = await this._genAI.models.generateContent({
+                        model: 'gemini-2.0-flash',
+                        contents: [
+                            createUserContent([
+                                request.Prompt,
+                                imagePart,
+                            ]),
+                        ],
+                    });
                     fs.unlink(request.Image.Path, (err) => {
                         if (err) throw err;
                         this._logger.Log(LogLevels.debug, `${request!.Image!.Path} was deleted successfully`);
                     });
                 } else
-                    result = await this._model.generateContent(request.Prompt);
-                response = new UseCaseResponseMessage("", true, result.response.text(), errors);
-                this._logger.Log(LogLevels.debug, `Prompt: ${request.Prompt} generates: ${response.Message}`);
+                    result = await this._genAI.models.generateContent({
+                        model: 'gemini-2.0-flash',
+                        contents: request.Prompt
+                    });
+                if (result && result.text) {
+                    let text = result.text.replace('```json', '')
+                    text = text.replace('```', '')
+                    response = new UseCaseResponseMessage("", true, result.text, errors);
+                    this._logger.Log(LogLevels.debug, `Prompt: ${request.Prompt} generates: ${response.Message}`);
+                } else {
+                    this._logger.Log(LogLevels.error, `Prompt ${request.Prompt} fails to generate result!`);
+                    response = new UseCaseResponseMessage("", false, `Prompt ${request.Prompt} fails to generate result!`, errors);
+                }
             } else {
                 this._logger.Log(LogLevels.error, `Invalid prompt! ${request.Prompt}!`);
                 response = new UseCaseResponseMessage("", false, `Invalid prompt! ${request.Prompt}`, errors);
